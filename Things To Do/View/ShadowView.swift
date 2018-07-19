80e993765e8d94b105e1b4db956fb469b0f2817d
Jesse Elkharrat <jessenelk@gmail.com>
2018-07-19 12:01:11 -0400

    front end 7.19

--- Things To Do/View/ShadowView.swift
+++ Things To Do/View/ShadowView.swift
@@ -6,4 +6,16 @@
 //  Copyright © 2018 practice. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ShadowView: UIView {
+    
+    override func awakeFromNib() {
+        self.layer.shadowOpacity = 0.75
+        self.layer.shadowRadius = 5
+        self.layer.shadowColor = #colorLiteral(red: 0.06274510175, green: 0, blue: 0.1921568662, alpha: 1)
+        
+        super.awakeFromNib()
+    }
+    
+}
